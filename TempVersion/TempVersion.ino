b0137f5d075434ad9c774d6bef64cf197ca150e9
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2016-12-09 07:40:09 +0000

    Wrote Temperature and pH test codes.
    If you're working on those subsystems, just implement something simmilar - it doesn't use any delays, so it should just be as simple as copy paste and modify.

---
+++ TempVersion/TempVersion.ino
@@ -0,0 +1,38 @@
+unsigned long long startTime;
+unsigned long long currentTime;
+float refVal = 30;
+int actuator = 0;
+void setup() {
+
+	Serial.begin(9600);
+	startTime = millis();
+
+}
+
+
+void loop() {
+      refVal = constrain(refVal, 25, 35);
+       currentTime = millis();
+       	// Uncomment and comment as necassary
+	// Every loop read serial value, every 1/10 sec, send value
+	if(currentTime - startTime > 100) {
+                Serial.print("b");
+                Serial.print(" ");
+		Serial.print(refVal);
+		Serial.print(" ");
+		Serial.print(actuator);
+		Serial.print("\n");
+                startTime = millis();  
+	}
+	if(refVal < 30) actuator = 1;
+        else actuator = 0;
+	if(Serial.available() > 0) {refVal = Serial.parseFloat(); Serial.flush();}
+
+	// So *in theory* this code prints ref value every 100 ms, and reads
+	// every clock cycle if input, and sets the value it will print out to the input.
+     
+
+
+
+}
+
---
+++ pHVersion/pHVersion.ino
@@ -0,0 +1,39 @@
+unsigned long long startTime;
+unsigned long long currentTime;
+float refVal = 30;
+int actuator = 0;
+void setup() {
+
+	Serial.begin(9600);
+	startTime = millis();
+
+}
+
+
+void loop() {
+      refVal = constrain(refVal, 3, 8);
+       currentTime = millis();
+       	// Uncomment and comment as necassary
+	// Every loop read serial value, every 1/10 sec, send value
+	if(currentTime - startTime > 100) {
+                Serial.print("b");
+                Serial.print(" ");
+		Serial.print(refVal);
+		Serial.print(" ");
+		Serial.print(actuator);
+		Serial.print("\n");
+                startTime = millis();  
+	}
+	if(refVal < 4) actuator = 1;
+        else if (refVal > 6) actuator = -1;
+        else actuator = 0;
+	if(Serial.available() > 0) {refVal = Serial.parseFloat(); Serial.flush();}
+
+	// So *in theory* this code prints ref value every 100 ms, and reads
+	// every clock cycle if input, and sets the value it will print out to the input.
+     
+
+
+
+}
+
